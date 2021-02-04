35fd023c168254a0f1ce55008a6743e6bd09cd7f
Estrada <Estrada@DESKTOP-11JFHEG.fritz.box>
2021-02-04 11:45:56 -0600

    #1 -> Versión 1.0

--- src/hola/mundo/HolaMundo.java
+++ src/hola/mundo/HolaMundo.java
@@ -1,22 +1,22 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * En el presente código mostraremos las diferentes formas de mostrar un mensaje al usuario
  */
 package hola.mundo;
+// Importamos la libreria para utilizar las ventanas emergentes de JOptionPane
+import javax.swing.JOptionPane;
 
-/**
- *
- * @author Estrada
- */
 public class HolaMundo {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // En la siguiente linea mostramos el mensaje directo en la consola
         System.out.println("Hola Mundo");
+        
+        //Mostrando en una ventana emergente de JOptionPane
+        JOptionPane.showMessageDialog(null, "Hola Mundo");
     }
     
 }
+/*
+* La finalidad de este código es ver las formas de mostrar un mensaje al usuario y así el
+desarrollador realice un programa con estas bases.
+*/
